0b766fa3cfb6ec427f20103be619c00807968d77
SindhuJudison <samsv0511@gmail.com>
2022-02-17 11:54:49 +0530

    Git

---
+++ src/org/git/GitTest.java
@@ -0,0 +1,23 @@
+package org.git;
+
+public class GitTest {
+	
+	private void one() {
+		System.out.println("git 1");
+
+	}
+	private void two() {
+		System.out.println("git 2");
+
+	}
+	private void three() {
+		System.out.println("git 3");
+
+	}
+public static void main(String[] args) {
+	GitTest g =  new GitTest();
+	g.one();
+	g.two();
+	g.three();
+}
+}
